19fdff64132fbaedc52a24ef8e68189e46a0e87c
Brandon Sanders <98419474+bsands97@users.noreply.github.com>
2022-02-15 14:03:56 -0500

    Add files via upload

---
+++ Feb 15.R
@@ -0,0 +1,14 @@
+# Author: Sanders, Date: 02/15/2022, Purpose: To perform microarray normalization
+
+# Set the working directory to your .CEL older
+setwd("/Users/Brandon/Desktop/CEL")
+
+# Load package affy
+library(affy)
+
+# Reading the .Cel Files
+eset <- ReadAffy()
+
+
+
+
